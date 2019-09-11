54d4c4efe921553b23e521e18a21d2cf3785ee86
IsmaelFarah101 <ismaelomermohamed1@gmail.com>
2019-09-11 00:53:26 -0500

    Added a newline before and after each book is printed

--- ui.py
+++ ui.py
@@ -27,8 +27,9 @@ def show_books(books):
     # TODO print a blank line before and after the book list or no books message
 
     if books:
+        #add a newline before and after the book is printed
         for book in books:
-            print(book)
+            print("\n" + book +"\n")
     else:
         print('No books to display')
 
