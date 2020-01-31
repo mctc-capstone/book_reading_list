704bcd0329fc507e552394734110f5a4d55d56f2
crantz007 <youskant@gmail.com>
2020-01-30 22:24:53 -0600

    main.py:print confirmation when read status changed

--- main.py
+++ main.py
@@ -60,10 +60,14 @@ def search_book():
 def change_read():
 
     book_id = ui.get_book_id()
-    book = store.get_book_by_id(book_id)  
-    new_read = ui.get_read_value()     
-    book.read = new_read 
-    book.save()
+    book = store.get_book_by_id(book_id)
+    # checking the object type
+    if isinstance(book, Book):
+        new_read = ui.get_read_value()
+        book.read = new_read
+        book.save()
+    elif book == 'not found':
+        create_menu()
     
 
 def quit_program():
