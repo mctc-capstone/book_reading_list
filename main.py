38c97f781c179fc5aaf957a1c592862304bd32ab
unknown <fq3356sj@go.minnstate.edu>
2021-09-14 22:34:50 -0500

    added readme.txt

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ def create_menu():
     menu = Menu()
     menu.add_option('1', 'Add Book', add_book)
     menu.add_option('2', 'Search For Book', search_book)
-    menu.add_option('3', 'Show Unread Books', show_unread_books)
-    menu.add_option('4', 'Show Read Books', show_read_books)
-    menu.add_option('5', 'Show All Books', show_all_books)
-    menu.add_option('6', 'Change Book Read Status', change_read)
+    menu.add_option('3', 'Delete Book', delete_book)
+    menu.add_option('4', 'Show Unread Books', show_unread_books)
+    menu.add_option('5', 'Show Read Books', show_read_books)
+    menu.add_option('6', 'Show All Books', show_all_books)
+    menu.add_option('7', 'Change Book Read Status', change_read)
     menu.add_option('Q', 'Quit', quit_program)
 
     return menu
@@ -35,12 +36,21 @@ def add_book():
     new_book = ui.get_book_info()
     new_book.save()
     
+    
 
 def show_read_books():
     read_books = store.get_books_by_read_value(True)
     ui.show_books(read_books)
 
 
+def delete_book():
+    books = store.get_all_books()
+    ui.show_books(books)
+    book_id = ui.get_book_id()
+    book = store.get_book_by_id(book_id)
+    book.delete()
+    
+    
 def show_unread_books():
     unread_books = store.get_books_by_read_value(False)
     ui.show_books(unread_books)
@@ -55,6 +65,7 @@ def search_book():
     search_term = ui.ask_question('Enter search term, will match partial authors or titles.')
     matches = store.book_search(search_term)
     ui.show_books(matches)
+    
 
 
 def change_read():
